000960b27dc0ff86218aeb78f73eda5f8b9460b1
gabrieldev21 <gabrielp.amorim@login.com>
2022-02-14 15:25:13 -0300

    fix button component in login page and types

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from 'react';
 import { MdPerson, MdLockOutline } from 'react-icons/md';
