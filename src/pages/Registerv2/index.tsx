2ca9e06a8112b466fd467e3cc1f80c570eac415e
Daniel <danielcarlossantanna@gmail.com>
2022-02-26 18:20:38 -0300

    wip: add texts

--- src/pages/Registerv2/index.tsx
+++ src/pages/Registerv2/index.tsx
@@ -7,6 +7,12 @@ const Register = () => {
       <S.Image />
       <S.CutImage />
       <S.Logo />
+      <S.Container>
+        <S.Title>Create new account.</S.Title>
+        <S.Subtitle>
+          Already A Member? <S.LinkStyled to="/login">Log in</S.LinkStyled>
+        </S.Subtitle>
+      </S.Container>
     </S.Wrapper>
   );
 };
--- src/pages/Registerv2/styleds.ts
+++ src/pages/Registerv2/styleds.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 import imageSrc from 'assets/images/background-enter.jpg';
 import LogoSVG from './Logo';
 
@@ -11,19 +13,23 @@ export const Wrapper = styled.div`
 `;
 
 export const CutImage = styled.div`
-  margin: 0;
   width: 100vw;
   height: 100vh;
   position: absolute;
-  transform: skewY(65deg);
   background-color: hsl(232deg 16% 19%);
+  background: rgb(41, 45, 56);
+  background: linear-gradient(
+    45deg,
+    rgba(41, 45, 56, 1) 0%,
+    rgba(41, 43, 56, 1) 65%,
+    rgba(41, 43, 56, 0.5979342420561975) 100%
+  );
 `;
 
 export const Image = styled.img.attrs({ src: imageSrc })`
   position: absolute;
   right: 0;
   width: 50vw;
-  opacity: 0.1;
   filter: blur(1px);
 `;
 
@@ -31,4 +37,32 @@ export const Logo = styled(LogoSVG).attrs({ color: '#FFF' })`
   position: absolute;
   bottom: 10px;
   right: 10px;
+`;
+
+export const Title = styled.h1`
+  color: #fff;
+  font-size: 3rem;
+  margin: 0;
+`;
+
+export const LinkStyled = styled(Link)`
+  font-size: 24px;
+  text-decoration: none;
+  color: hsl(210deg 66% 33%);
+  font-weight: 600;
+`;
+export const Subtitle = styled.h2`
+  color: #727b88;
+  font-size: 24px;
+  font-weight: normal;
+`;
+
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-left: 40px;
+  position: relative;
+  z-index: 1;
 `;
