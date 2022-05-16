36ebf9145f4e87a850c6d4e174c15bf3cbdc69c1
gabrieldev21 <gabrielp.amorim@live.com>
2022-05-16 15:53:46 -0300

    feature: modal error in register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -19,7 +19,9 @@ import Unauthenticated, {
   FormRow,
   ButtonRow,
 } from 'components/templates/Unauthenticated';
+import Dialog from 'components/Dialog';
 import RegisterSchema from './RegisterSchema';
+import { ErrorButtonRegister, TextLoginError } from './styleds';
 
 type RegisterForm = {
   firstName: string,
@@ -36,6 +38,7 @@ const Register = () => {
   });
   const history = useHistory();
   const { t } = useTranslation();
+  const [openDialogRegisterError, setOpenDialogRegisterError] = useState(false);
 
   const onSubmit = async (data: RegisterForm) => {
     setLoading(true);
@@ -46,13 +49,14 @@ const Register = () => {
       if (axios.isAxiosError(error)) {
         const responseMessageError = error.response?.data.errors;
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        Object.keys(responseMessageError).forEach((errorKey: any) => {
+        Object.keys(responseMessageError || {}).forEach((errorKey: any) => {
           setError(errorKey, {
             message: responseMessageError[errorKey].message,
           });
         });
       }
       setLoading(false);
+      setOpenDialogRegisterError(true);
     }
   };
 
@@ -95,6 +99,15 @@ const Register = () => {
             icon={<BsFillEyeFill />}
           />
         </FormRow>
+        <Dialog
+          isOpen={openDialogRegisterError}
+          onClose={() => setOpenDialogRegisterError(false)}
+          width="400px"
+          hideCloseButton
+        >
+          <TextLoginError>{t('signup.error')}</TextLoginError>
+          <ErrorButtonRegister onClick={() => setOpenDialogRegisterError(false)}>OK</ErrorButtonRegister>
+        </Dialog>
         <ButtonRow>
           <Button disabled={!formState.isValid} type="submit">
             {t('signup.buttonSubmit')}
---
+++ src/pages/Register/styleds.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import Button from 'components/Button';
+
+export const TextLoginError = styled.div`
+  font-size: 20px;
+  text-align: center;
+`;
+
+export const ErrorButtonRegister = styled(Button)`
+  margin-top: 8px;
+  padding: 16px 40px;
+`;
